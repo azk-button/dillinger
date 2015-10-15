dc6ab12d0f6b54fcdef1f379abc267902852753e
Julio Makdisse Saito <saitodisse@gmail.com>
2015-10-15 00:16:27 -0300

    [azk] manual fix on gulp-group-css-media-queries (FIXME)

--- Azkfile.js
+++ Azkfile.js
@@ -4,17 +4,24 @@
 systems({
   dillinger: {
     depends: [],
-    image: { docker: 'node:latest' },
+    image: { docker: 'azukiapp/node:4' },
     provision: [
       'npm install',
+      'npm install gulp',
+
+      // FIXME: remove this after gulp-group-css-media-queries
+      // is updated. https://github.com/SE7ENSKY/group-css-media-queries/pull/8
+      'wget https://raw.githubusercontent.com/saitodisse/group-css-media-queries/71fe8b181650b20790aca325988ac0fb9d9fe4a4/index.js -O ./node_modules/gulp-group-css-media-queries/node_modules/group-css-media-queries/index.js',
+      'wget https://raw.githubusercontent.com/saitodisse/group-css-media-queries/71fe8b181650b20790aca325988ac0fb9d9fe4a4/index.js -O ./node_modules/group-css-media-queries/index.js',
+
       'node_modules/.bin/gulp build --prod',
     ],
     workdir: '/azk/#{manifest.dir}',
     shell: '/bin/bash',
-    command: 'node app',
+    command: 'NODE_ENV=production node app',
     wait: 30,
     mounts: {
-      '/azk/#{manifest.dir}': path('.'),
+      '/azk/#{manifest.dir}': sync('.'),
       '/azk/#{manifest.dir}/node_modules': persistent('#{manifest.dir}/node_modules'),
       '/azk/#{manifest.dir}/bower_components': persistent('#{manifest.dir}/bower_components'),
     },
@@ -31,7 +38,7 @@ systems({
     },
     envs: {
       PATH: 'node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      NODE_ENV: 'production',
+      // NODE_ENV: 'production',
       HOST_NAME: '#{system.name}.#{azk.default_domain}',
       PORT: '8080',
     }
